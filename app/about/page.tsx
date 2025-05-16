ae43b15b52cb3e387712dd95d8fa0ab32a86cc59
Arpan Gohe <166099844+Arpangohe@users.noreply.github.com>
2025-05-16 11:09:43 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,21 @@ import CallToAction from "@/components/call-to-action"
 export default function AboutPage() {
   return (
     <>
-      <PageHero title="about_title" description="asp" src="https://i.ibb.co/sJkB90BZ/about-1-11zon.jpg" />
+      <div 
+        className="fixed top-0 left-0 w-full h-full -z-10"
+        style={{
+          backgroundImage: "url('https://i.ibb.co/sJkB90BZ/about-1-11zon.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundAttachment: "fixed"
+        }}
+      />
+      <PageHero 
+        title="about_title" 
+        description="asp" 
+        backgroundImage="https://i.ibb.co/sJkB90BZ/about-1-11zon.jpg"
+      />
+{/*       <PageHero title="about_title" description="asp" src="https://i.ibb.co/sJkB90BZ/about-1-11zon.jpg" /> */}
       <OurStory />
       <WhyChooseUs />
       <SectorsDetailed />
